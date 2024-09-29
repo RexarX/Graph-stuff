7c13afc2a3495ef8cde1d57eeb72cf3d75068da6
Denis <96687186+RexarX@users.noreply.github.com>
2024-09-30 00:00:36 +0300

    Update 140.cpp

--- 140.cpp
+++ 140.cpp
@@ -31,9 +31,9 @@ int main()
   std::cin >> n;
 
   std::vector<std::vector<int>> graph(n, std::vector<int>(n));
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n; ++j) {
-      std::cin >> graph[i][j];
+  for (auto& vec : graph) {
+    for (int& value : vec) {
+      std::cin >> value
     }
   }
 
