f348b674e5e0cf715317416c0e64f9431ae5288f
Denis <96687186+RexarX@users.noreply.github.com>
2024-09-30 17:18:01 +0300

    Update 15.cpp

--- 15.cpp
+++ 15.cpp
@@ -3,13 +3,12 @@
 int main()
 {
   std::ios_base::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
-   
-  int count = 0;
-   
+ 
   int size = 0;
   std::cin >> size;
   size *= size;
-  
+
+  int count = 0;
   int num = 0;
   for (int i = 0; i < size; ++i) {
     std::cin >> num;
