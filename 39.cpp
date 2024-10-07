3bc92a296ce3932a75715dcda32263922bdbbe12
Denis <96687186+RexarX@users.noreply.github.com>
2024-10-07 14:58:41 +0300

    Update 39.cpp

--- 39.cpp
+++ 39.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-
+ 
 int main()
 {
   std::ios_base::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
-
+ 
   int n = 0;
   std::cin >> n;
-
+ 
   std::vector<int> prices(n);
   for (int& price : prices) {
     std::cin >> price;
   }
-
+ 
   std::vector<int> dp(n + 1, 0);
   for (int day = n - 1; day >= 0; --day) {
     int maxProfit = 0;
     for (int sellDay = day; sellDay < n; ++sellDay) {
       int profit = prices[sellDay] * (sellDay - day + 1) + dp[sellDay + 1];
-      maxProfit = std::max(maxProfit, profit);
+      if (maxProfit < profit) { maxProfit =  profit; }
     }
     dp[day] = maxProfit;
   }
-
+ 
   std::cout << dp[0];
-
+ 
   return 0;
 }
